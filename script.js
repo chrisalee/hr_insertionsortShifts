98e5e7ef3ced9ace7a592fb9fa38347a677ee978
Christopher L <56335217+chrisalee@users.noreply.github.com>
2023-04-25 08:41:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,57 @@ function insertionSort(arr) {
 }
 
 ////////////////////////////////////////////////////////////
+const mergeAndCount = (arr, left, mid, right) => {
+    // left subarray
+    let leftArr = [];
+    for(let i = left; i < mid + 1; i++) {
+        leftArr.push(arr[i]);
+    }
+    
+    // right subarray
+    let rightArr = [];
+    for(let i = mid + 1; i < right + 1; i++) {
+        rightArr.push(arr[i]);
+    }
+    
+    let i = 0;
+    let j = 0;
+    let k = left;
+    let swaps = 0;
+    
+    while(i < leftArr.length && j < rightArr.length) {
+        if(leftArr[i] <= rightArr[j]) arr[k++] = leftArr[i++];
+        else {
+            arr[k++] = rightArr[j++];
+            swaps += (mid + 1) - (left + i);
+        }
+    }
+    
+    while(i < leftArr.length) {
+        arr[k++] = leftArr[i++];
+    }
+    while(j < rightArr.length) {
+        arr[k++] = rightArr[j++];
+    }
+    // console.log('swaps ********', swaps);
+    return swaps;
+}
+
+// this is actually a merge sort function, add left and right variables in arguments
+function insertionSort(arr, left = 0, right = arr.length - 1) {
+    // keeps track of the inversion count at a particular node of the recursion tree
+    let count = 0
+    if(left < right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        // total inversion count = left subarray count + right subarray count + merge count
+        
+        // left subarray count
+        count += insertionSort(arr, left, mid)
+        // right subarray count
+        count += insertionSort(arr, mid + 1, right);
+        // merge count
+        count += mergeAndCount(arr, left, mid, right);        
+    }
+    return count;
+}
